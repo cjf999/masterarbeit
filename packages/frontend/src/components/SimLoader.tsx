0264f105b0cdece56fef9df87e8f926b4122531d
Carl Fedrowitz <carl.fedrowitz@student.uni-luebeck.de>
2026-02-16 17:42:24 +0100

    added SimLoader (flexible component for fetching Sims), more coming

---
+++ packages/frontend/src/components/SimLoader.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+//import other components
+import Footer from "./footer";
+import Navbar from "./Navbar";
+
+export default function SimLoader(){
+    
+    //holt state prop aus location um {id} anhängen zu können
+    const { state } = useLocation(); 
+
+    //simulationId auf leeren string setzen
+    const [simulationId, setSimulationId] = useState(""); 
+    const [data, setData] = useState(null);
+
+    //wenn sich sim id ändert, dann neue daten mit neuer sim id fetchen
+    useEffect(() => {
+        fetchData(simulationId);
+    }, [simulationId])
+
+    //wenn sich state ändert, dann setze simulationId auf state.simId (sofern da)
+    useEffect(() => {
+        setSimulationId(state?.simId || "");
+    }, [state])
+
+    //api call
+    const fetchData = (simId: string | null) => {
+        if (!simId) return;
+        console.log("fetching", "https://gems.hciuse.sh/simulations/" + simId)
+        fetch("https://gems.hciuse.sh/simulations/" + simId).then(async res => {
+            setData(await res.json())
+        });
+    }
+
+    //debugging
+    useEffect(() => {
+        console.log('data:', data);
+    }, [data])
+
+    //submithandler 
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const data = new FormData(e.currentTarget);
+        const simId = data.get("id") as string;
+        console.log('looking for simid', simId);
+        setSimulationId(simId);
+    }     
+
+    return(
+        <>
+            <Navbar /> 
+            <h1>SimLoader-Component</h1>
+            <form onSubmit={handleSubmit} className="result-form">
+                <div>
+                    {
+                        simulationId ? <input name="id" type="text" value={simulationId} /> : <input name="id" type="text" />
+                    }
+                </div>
+            </form>
+            <Footer />
+        </>
+        )
+}
--- packages/frontend/src/components/footer.tsx
+++ packages/frontend/src/components/footer.tsx
@@ -1,6 +1,6 @@
 function Footer(){
     return(
-        <div className="footer">
+        <div className="Footer">
             <div className="footer-content">
                 <img id="uzl_svg" src="src/assets/uzl_logo.svg" alt="uzl_logo" />
                 <p>Dieses Projekt ist im Rahmen einer Masterabeit an der Universität zu Lübeck entstanden.</p>
