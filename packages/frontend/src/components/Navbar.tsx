90843925c1a208499d690741276055f8e4220fd4
Carl Fedrowitz <carl.fedrowitz@student.uni-luebeck.de>
2026-02-18 00:55:09 +0100

    minor navbar cleanup, added active class, added link to home on h2, removed old styling, added new styling

--- packages/frontend/src/components/Navbar.tsx
+++ packages/frontend/src/components/Navbar.tsx
@@ -1,36 +1,29 @@
-import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { Link, useMatch, useResolvedPath, NavLink, Navigate, useNavigate } from "react-router-dom";
 
 export default function Navbar(){
+
+    const navigate = useNavigate();
+    const routeChange = () =>{
+        const path = "/";
+        navigate(path)
+    }
+
     return(
         <nav className="navbar">
             <div className="navbar-content">
+                { /*
                 <Link className="setup-link" to="/setup" >
                     Setup
                 </Link>
-                <h2>GEMS Interface</h2>
+                */}
+                <h2 onClick={routeChange} style={{cursor: "pointer"}}>GEMS Interface</h2>
                 <ul>
-                    <CustomLink to="/result">Result</CustomLink>
-                    <CustomLink to="/compare">Compare</CustomLink>    
-                    <CustomLink to="/history">History</CustomLink>
+                    <NavLink className="nav-link" to="/setup">Setup</NavLink>
+                    <NavLink className="nav-link" to="/result">Result</NavLink>
+                    <NavLink className="nav-link" to="/compare">Compare</NavLink>    
+                    <NavLink className="nav-link" to="/history">History</NavLink>
                 </ul>
             </div>
         </nav>
-    )
-}
-
-//TODO: reparieren, funktioniert so nicht
-function CustomLink({ to, children, ...props}: {to: any, children: any}){
-
-    const resolvedPath = useResolvedPath(to);
-    const isActive = useMatch({path: resolvedPath.pathname, end: true});
-
-    return(
-        <>
-            <li className={isActive ? "active": ""}>
-                <Link to={to} {...props}>
-                    {children}
-                </Link>
-            </li>
-        </>
     )
 }
