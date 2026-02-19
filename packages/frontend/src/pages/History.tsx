e786a998f52f137639ba42a0f7d3c480f92287d4
Carl Fedrowitz <carl.fedrowitz@student.uni-luebeck.de>
2026-02-19 15:49:32 +0100

    added react mui component library, added history dummy content as table

--- packages/frontend/src/pages/History.tsx
+++ packages/frontend/src/pages/History.tsx
@@ -1,62 +1,49 @@
 //import Components
-
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 //import { useEffect, useState } from "react";
+import { Table, TableHead, TableBody, TableContainer, TableRow, TableCell } from "@mui/material";
 
 //import Routes
-import { Link } from "react-router-dom";
-
-/** 
-interface Simulation{
-    id: string;
-    name: string;
-}
-*/
 
 const History = () => {
 
-    /** 
-    const [testusers, setTestusers] = useState<Simulation[]>([]);
+    //creating dummy simulationsdaten
+    function createData(
+        id: string,
+        seed: number,
+        individuals: number,
+        infections: number,
+        householdSize: number,
+        transmissionRate: number,
+        reproductionNumber: number,
+        attackRate: number,
+        startDate: string,
+        endDate: string,
+    ){
+        return{id, seed, individuals, infections, householdSize, transmissionRate, reproductionNumber, attackRate, startDate, endDate};
+    }
 
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-        .then((response) => response.json())
-        .then((data) => setTestusers(data))
-    
-    })
-    */
+    const rows = [
+        createData("sim_1771331080051", 420, 10000, 4562, 1, 0.3, 0.8, 0.45, "01.01.2025", "01.07.2025"),
+        createData("sim_0000000000000", 201, 10000, 2000, 1, 0.2, 0.7, 0.46, "01.01.2025", "01.07.2025"),
+        createData("sim_0000000000001", 202, 11000, 2000, 1, 0.2, 0.6, 0.47, "01.01.2025", "01.07.2025")
+    ]
 
-    //array aus simulationen mit id
+    //dummy content: array aus simulationen mit id
+    {/** 
     const simulationen = [
         { id: 1, title: "Simulation 1"},
         { id: 2, title: "Simulation 2"},
         { id: 3, title: "Simulation 3"}
     ]
-
+    */}
 
     return(
     <>
         <Navbar />
-        <h1>Vergangene Simulationen</h1>
-        
-        {/* 
-            <h2>(dummy content f. test von api)</h2>
-        <ul className="api-test">
-            {testusers.map((testusers) => (
-                <li key={testusers.id} >
-                    <Link to={`/history/${testusers.id}`}>
-                        <h2>{testusers.name}</h2>
-                    </Link>
-                    <p>Testuser-ID ist {testusers.id}</p>
-                    
-                </li>
-            ))}
-        </ul>
-        */}
-        
-
-        
+        <h2>Vergangene Simulationen</h2>
+        {/** 
         <ul className="ul-history" style={{listStyle: "none", display: "flex", justifyContent: "space-around"}}>
             {simulationen.map((item) => ( //gehe über das array, gib pro id die entsprechende sim aus
                 <li key={item.id} className="li-history">
@@ -66,6 +53,44 @@ const History = () => {
                 </li>
             ))}
         </ul>
+        */}
+
+        <div className="table-history" style={{textAlign: "center", display: "flex",alignItems: "center", justifyContent: "center"}}>
+            <TableContainer style={{maxWidth: "1200px", fontSize: "1rem"}}> 
+            <Table aria-label="simple table" align="center" stickyHeader>
+            <TableHead>
+                <TableRow sx={{backgroundColor: "#E1E4E8"}}>
+                    <TableCell >ID</TableCell>
+                    <TableCell>Seed</TableCell>
+                    <TableCell>Individuals</TableCell>
+                    <TableCell>Infections</TableCell>
+                    <TableCell>Household Size</TableCell>
+                    <TableCell>Transmission Rate</TableCell>
+                    <TableCell>Reproduction Number</TableCell>
+                    <TableCell>Attack Rate</TableCell>
+                    <TableCell>Start Date</TableCell>
+                    <TableCell>End Date</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {rows.map((row) => (
+                    <TableRow key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                        <TableCell component="th" scope="row">{row.id}</TableCell>
+                        <TableCell align="right">{row.seed}</TableCell>
+                        <TableCell align="right">{row.individuals}</TableCell>
+                        <TableCell align="right">{row.infections}</TableCell>
+                        <TableCell align="right">{row.householdSize}</TableCell>
+                        <TableCell align="right">{row.transmissionRate}</TableCell>
+                        <TableCell align="right">{row.reproductionNumber}</TableCell>
+                        <TableCell align="right">{row.attackRate}</TableCell>
+                        <TableCell align="right">{row.startDate}</TableCell>
+                        <TableCell align="right">{row.endDate}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+            </Table>
+            </TableContainer>
+        </div>
         <Footer />
     </>
     )
