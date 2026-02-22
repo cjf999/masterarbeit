58a0db64a4f56f888e56432c91b144f849a91777
Carl Fedrowitz <carl.fedrowitz@student.uni-luebeck.de>
2026-02-22 16:59:55 +0100

    edited start page for clear user flows

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 //import dependencies 
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import './App.css'
 //import other components
 import Navbar from './components/Navbar';
@@ -8,7 +8,7 @@ import Footer from './components/Footer';
 function App() {
  
   const location = useLocation();
-
+  const navigate = useNavigate();
   const clickHandler = () => {
     console.log(location.pathname);
   }  
@@ -16,10 +16,29 @@ function App() {
   return (
     <>
     <Navbar />
-      <h1>Vite + React</h1>
-    <div className='default-content'>
-      <button onClick={clickHandler}>Gib aktuelle url aus</button>
+    <h1>Willkommen!</h1>
+    <h2>Wie möchten Sie fortfahren?</h2>
+    <div className="main-container">
+      <div className="left-container">
+        <div className="content" onClick={() => navigate("/setup")}>
+          <h3>Möchten Sie eine neue Simulation starten?</h3>
+          <img src="/src/assets/newSim.svg" alt="start new simulation" />
+        </div>
+      </div>
+      <div className="center-container">
+        <div className="content" onClick={() => navigate("/result")}>
+          <h3>Möchten Sie eine vorherige Simulation laden?</h3>
+          <img src="/src/assets/loadSim.svg" alt="load previous simulation" />
+        </div>
+      </div>
+      <div className="right-container">
+        <div className="content" onClick={() => navigate("/compare")}>
+          <h3>Möchten Sie zwei Simulationen vergleichen?</h3>
+          <img src="/src/assets/compareSim.svg" alt="compare two simulations" />
+        </div>
+      </div>
     </div>
+    <button onClick={clickHandler}>Gib aktuelle url aus</button>
     <Footer />
     </>
   )
