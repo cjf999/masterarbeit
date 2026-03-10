1a65ae0adadaee4715e1fe03961899876c9c74a3
Maged Mortaga <maged.mortaga@uni-luebeck.de>
2026-03-10 10:55:41 +0100

    feat: make project docker ready

--- packages/frontend/src/pages/ResultItems.tsx
+++ packages/frontend/src/pages/ResultItems.tsx
@@ -1,12 +1,3 @@
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
-
-import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
-
-
-export default function ResultItems(){
-    return(
-        <div>items</div>
-    )
+export default function ResultItems() {
+  return <div>items</div>;
 }
