7677c3ecde56bfc3bfa0e488f6ae51b69d94eb48
Maged Mortaga <maged.mortaga@uni-luebeck.de>
2026-03-10 11:29:57 +0100

    fix: update image paths to also load in prod

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,44 +1,47 @@
-//import dependencies 
-import { useNavigate } from 'react-router-dom';
-import './App.css'
+//import dependencies
+import { useNavigate } from "react-router-dom";
+import "./App.css";
 //import other components
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+// Svgs
+import compareSimSrc from "./assets/compareSim.svg";
+import loadSimSrc from "./assets/loadSim.svg";
+import newSimSrc from "./assets/newSim.svg";
 
 function App() {
- 
   const navigate = useNavigate();
 
   return (
     <>
-    <Navbar />
-    <div className="page-container">
-    <h1>Willkommen!</h1>
-    <h2>Wie möchten Sie fortfahren?</h2>
-    <div className="main-container">
-      <div className="left-container">
-        <div className="content" onClick={() => navigate("/setup")}>
-          <h3>Möchten Sie eine neue Simulation starten?</h3>
-          <img src="/src/assets/newSim.svg" alt="start new simulation" />
+      <Navbar />
+      <div className="page-container">
+        <h1>Willkommen!</h1>
+        <h2>Wie möchten Sie fortfahren?</h2>
+        <div className="main-container">
+          <div className="left-container">
+            <div className="content" onClick={() => navigate("/setup")}>
+              <h3>Möchten Sie eine neue Simulation starten?</h3>
+              <img src={newSimSrc} alt="start new simulation" />
+            </div>
+          </div>
+          <div className="center-container">
+            <div className="content" onClick={() => navigate("/result")}>
+              <h3>Möchten Sie eine vorherige Simulation laden?</h3>
+              <img src={loadSimSrc} alt="load previous simulation" />
+            </div>
+          </div>
+          <div className="right-container">
+            <div className="content" onClick={() => navigate("/compare")}>
+              <h3>Möchten Sie zwei Simulationen vergleichen?</h3>
+              <img src={compareSimSrc} alt="compare two simulations" />
+            </div>
+          </div>
         </div>
       </div>
-      <div className="center-container">
-        <div className="content" onClick={() => navigate("/result")}>
-          <h3>Möchten Sie eine vorherige Simulation laden?</h3>
-          <img src="/src/assets/loadSim.svg" alt="load previous simulation" />
-        </div>
-      </div>
-      <div className="right-container">
-        <div className="content" onClick={() => navigate("/compare")}>
-          <h3>Möchten Sie zwei Simulationen vergleichen?</h3>
-          <img src="/src/assets/compareSim.svg" alt="compare two simulations" />
-        </div>
-      </div>
-    </div>
-    </div>
-    <Footer />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- packages/frontend/src/components/footer.tsx
+++ packages/frontend/src/components/footer.tsx
@@ -1,14 +1,20 @@
-function Footer(){
-    return(
-        <div className="footer">
-            <div className="footer-content">
-                <img id="uzl_svg" src="/src/assets/uzl_logo.svg" alt="uzl_logo" />
-                <p>Dieses Projekt ist im Rahmen einer Masterabeit an der Universität zu Lübeck entstanden.</p>
-                <a id="impressum" target="_blank" href="https://www.imis.uni-luebeck.de/de/impressum">Impressum</a>
-                <a id="datenschutz" target="_blank" href="https://www.imis.uni-luebeck.de/de/datenschutz">Datenschutz</a>
-            </div>
-        </div>
-    )
+import logoSrc from "../assets/uzl_logo.svg";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <img id="uzl_svg" src={logoSrc} alt="uzl_logo" />
+        <p>Dieses Projekt ist im Rahmen einer Masterabeit an der Universität zu Lübeck entstanden.</p>
+        <a id="impressum" target="_blank" href="https://www.imis.uni-luebeck.de/de/impressum">
+          Impressum
+        </a>
+        <a id="datenschutz" target="_blank" href="https://www.imis.uni-luebeck.de/de/datenschutz">
+          Datenschutz
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
